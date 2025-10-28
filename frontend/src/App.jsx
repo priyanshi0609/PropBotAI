89519c04c31db576ecb150a1cfb82071a1232521
Priyanshi Bothra <priyanshibothra753@gmail.com>
2025-10-28 18:23:22 +0530

    fixed tailwind issue

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import ChatInterface from './components/ChatInterface';
+import ChatInterface from './components/ChatInterFace.jsx';
 import './styles/globals.css';
 
 function App() {
