13a8c2016056765b4ea03e6da32d65184285bbab
Priyanshi Bothra <priyanshibothra753@gmail.com>
2025-10-30 01:50:14 +0530

    firebase setup

---
+++ frontend/nobrokereageai-frontend/src/services/firebase.js
@@ -0,0 +1,24 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getAuth } from 'firebase/auth';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDf29Yo9orAoMK45WpzMHqMSlg8cVpMl9E",
+  authDomain: "nobrokerageai.firebaseapp.com",
+  projectId: "nobrokerageai",
+  storageBucket: "nobrokerageai.firebasestorage.app",
+  messagingSenderId: "908054666642",
+  appId: "1:908054666642:web:8e106670a723a23f58354b",
+  measurementId: "G-R8XVXD5V9Y"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+export default app;
